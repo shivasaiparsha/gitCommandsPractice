340ba8ef34621e79bb873f38c1bd74ba5dd4e9a9
shivasaiparsha <112009608+shivasaiparsha@users.noreply.github.com>
2025-01-12 16:05:59 +0530

    added print statement

---
+++ GitCommandsPractice.java
@@ -0,0 +1,6 @@
+public class GitCommandsPractice {
+     public static void main(String[] args) {
+        System.out.println("git comit --amend ");
+        System.out.println("adding code to last commit ");
+    }
+}
