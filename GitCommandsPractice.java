e54e94e7c1fe888a74d2b2973c969b10dc9a1eb0
shivasaiparsha <112009608+shivasaiparsha@users.noreply.github.com>
2025-01-12 22:04:12 +0530

    revert commit

--- GitCommandsPractice.java
+++ GitCommandsPractice.java
@@ -6,5 +6,6 @@ public class GitCommandsPractice {
         System.out.println("adding code to existing commit");
         System.out.println("added new commit");
         System.out.println("switched cherry pic branch");
+        System.out.println("revert");
     }
 }
