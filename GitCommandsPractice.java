f98cb9ca0b9fced9af55b0134ba41ca6b6cb80b3
shivasaiparsha <112009608+shivasaiparsha@users.noreply.github.com>
2025-01-12 16:28:48 +0530

    commit on new branch

--- GitCommandsPractice.java
+++ GitCommandsPractice.java
@@ -1,7 +1,5 @@
 public class GitCommandsPractice {
      public static void main(String[] args) {
-        System.out.println("git comit --amend ");
-        System.out.println("adding code to existing commit");
-        System.out.println("added new commit");
+        System.out.println("new branch is created under main");
     }
 }
